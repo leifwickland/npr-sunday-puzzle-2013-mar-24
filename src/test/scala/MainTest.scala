e48bfd59bef34d53e0932644b14a5a590b576814
Leif Wickland <leifwickland@gmail.com>
2013-03-24 16:14:11 -0600

    Solved

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -4,7 +4,21 @@ import org.scalatest.matchers.ShouldMatchers
 import Main._
 
 class MainTest extends FunSuite with ShouldMatchers {
-  test("something") {
-    1.toString should be("1")
+  test("given 4 letter case") {
+    Square.place(List(
+      List("salt"),
+      List("afar"),
+      List("lava"),
+      List("trap")
+    )) should be(Seq(Square(Seq("salt", "afar", "lava", "trap"))))
+  }
+
+  test("given 4 letter case with herrings") {
+    Square.place(List(
+      List("salt", "asdf"),
+      List("afar", "qewr"),
+      List("lava", "zxcv"),
+      List("trap", "pius")
+    )) should be(Seq(Square(Seq("salt", "afar", "lava", "trap"))))
   }
 }
